efe7dc4e981c54cf3d85ac6c268f969ba1b4d380
Mohammad Rahmati <mohammad.rahmati.ml@gmail.com>
2023-05-02 22:53:43 +0200

    handled title bug

--- extract_subtitles.py
+++ extract_subtitles.py
@@ -2,10 +2,11 @@ import sys
 import os
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
-
+import time
 
 def extract_subtitles(url: str) -> (str, str):
     video_id = YouTube(url).video_id
+    counter = 0
     while True:
         try:
             title = YouTube(url).title
@@ -14,6 +15,12 @@ def extract_subtitles(url: str) -> (str, str):
             print(f"{GREEN}Title: {title}{RESET}")
             break
         except Exception as e:
+            time.sleep(0.1)
+            counter += 1
+            if counter > 10:
+                print('could not get title.')
+                title = ""
+                break
             pass
 
     try:
